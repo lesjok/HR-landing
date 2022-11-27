0e857d31cf00977793b0efca6a62d8c5d5da6f74
lesjok <lesjok@inbox.ru>
2022-11-27 16:12:43 +0300

    add fonts, constants, eslint

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,1 @@
 import './index.css';
-import { test } from '../scripts/utils/constants';
-import photo from '../images/photo.jpg';
-// eslint-disable-next-line import/extensions
-import '../partJS/header.js';
-
-// eslint-disable-next-line no-undef
-const testPhoto = document.querySelector('.test_img');
-testPhoto.src = photo;
-testPhoto.alt = test;
