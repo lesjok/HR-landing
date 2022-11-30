0691cf0de84b010a25a3014c7bd7d49a4c1e4432
Dimitry-prog <kfifa3@gmail.com>
2022-12-01 00:24:54 +0300

    feat: form valid

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,14 @@ import {
   burgerButton,
   headerNav,
   quizReviewerBtn,
-  quizTeacherBtn
+  quizTeacherBtn,
+  formSteps,
+  multiStepForm,
+  formConfig,
+  peopleRole,
+  peopleContainer,
+  peopleDirection,
+  peopleDirectionContainer
 } from '../scripts/utils/constants';
 
 import { getCoordinate, scrollCards } from '../scripts/utils/slider';
@@ -47,3 +54,150 @@ if (clickCount > 5) {
 burgerButton.addEventListener('click', () => {
   headerNav.classList.toggle('header__nav_active');
 });
+
+
+let currentStep = Array.from(formSteps).findIndex(step => {
+  return step.classList.contains("form__step_active")
+});
+
+const hideAnimation = () => {
+  Array.from(formSteps).forEach(step => {
+    step.addEventListener("animationend", e => {
+      Array.from(formSteps)[currentStep].classList.remove("hide");
+      e.target.classList.toggle("hide", !e.target.classList.contains("form__step_active"));
+    });
+  });
+}
+
+Array.from(formSteps).forEach(step => {
+  step.addEventListener("animationend", e => {
+    Array.from(formSteps)[currentStep].classList.remove("hide");
+    e.target.classList.toggle("hide", !e.target.classList.contains("form__step_active"));
+  });
+});
+
+const showCurrentStep = () => {
+  //hideAnimation()
+  Array.from(formSteps).forEach((step, index) => {
+    step.classList.toggle("form__step_active", index === currentStep);
+  });
+  console.log(Array.from(formSteps)[currentStep])
+
+};
+
+if (currentStep < 0) {
+  currentStep = 0;
+  showCurrentStep();
+}
+
+multiStepForm.addEventListener("click", e => {
+
+  const inputs = [...Array.from(formSteps)[currentStep].querySelectorAll("input")]
+  const allValid = inputs.every(input => input.checkValidity());
+
+  if (e.target.matches("[data-next]") && allValid) {
+    currentStep = 1;
+    showCurrentStep()
+  } else if (e.target.matches("[data-previous]")) {
+    currentStep -= 1
+    showCurrentStep()
+  }
+
+})
+
+const showErrorMark = (inputElement) => {
+  const svgIcon = document.querySelector(`.${inputElement.id}-fail`);
+  svgIcon.classList.add('active');
+}
+
+const hideErrorMark = (inputElement) => {
+  const svgIcon = document.querySelector(`.${inputElement.id}-fail`);
+  svgIcon.classList.remove('active');
+}
+
+const showSuccessMark = (inputElement) => {
+  const svgIcon = document.querySelector(`.${inputElement.id}-success`);
+  svgIcon.classList.add('active');
+}
+
+const hideSuccessMark = (inputElement) => {
+  const svgIcon = document.querySelector(`.${inputElement.id}-success`);
+  svgIcon.classList.remove('active');
+}
+
+function showErrorMessage(formElement, inputElement, config) {
+  const error = formElement.querySelector(`.${inputElement.id}-error`);
+  error.classList.add(config.formErrorClass);
+  error.textContent = inputElement.validationMessage;
+  inputElement.classList.add(config.inputErrorClass);
+}
+
+function hideErrorMessage(formElement, inputElement, config) {
+  const error = formElement.querySelector(`.${inputElement.id}-error`);
+  error.classList.remove(config.formErrorClass);
+  error.textContent = '';
+  inputElement.classList.remove(config.inputErrorClass);
+}
+
+function hasValidInput(inputlist) {
+  return inputlist.some(input => {
+    return !input.validity.valid
+  })
+}
+
+function checkInputValidity(formElement, inputElement, config) {
+  if (!inputElement.validity.valid) {
+    showErrorMessage(formElement, inputElement, config);
+    showErrorMark(inputElement);
+    hideSuccessMark(inputElement);
+    inputElement.classList.remove(config.inputSuccessClass);
+  } else {
+    hideErrorMessage(formElement, inputElement, config);
+    hideErrorMark(inputElement);
+    showSuccessMark(inputElement);
+    inputElement.classList.add(config.inputSuccessClass);
+  }
+}
+
+function getInputListAndSubmitButton(formElement, config) {
+  const inputlist = Array.from(formElement.querySelectorAll(config.inputSelector));
+  return inputlist
+}
+
+function resetImputsErrorMessage(formElement, config) {
+  const errors = Array.from(formElement.querySelectorAll(config.formErrorMessageSelector));
+  errors.forEach(error => {
+    error.classList.remove(config.formErrorClass);
+  });
+
+  getInputListAndSubmitButton(formElement, config);
+
+  getInputListAndSubmitButton(formElement, config).forEach(input => {
+    input.classList.remove(config.inputErrorClass);
+  })
+}
+
+function setEvenetListeners(formElement, config) {
+  getInputListAndSubmitButton(formElement, config);
+  getInputListAndSubmitButton(formElement, config).forEach(input => {
+    input.addEventListener('blur', function () {
+      checkInputValidity(formElement, input, config);
+    });
+  });
+}
+
+function enableFormValidation(config) {
+  const form = document.querySelector(config.formSelector);
+  setEvenetListeners(form, config);
+}
+
+enableFormValidation(formConfig);
+
+
+peopleRole.addEventListener('click', () => {
+  peopleContainer.classList.toggle('active')
+})
+
+peopleDirection.addEventListener('click', () => {
+  peopleDirectionContainer.classList.toggle('active')
+})
